061d388006f36740e5f34d294af4940a7d7b148a
San-Miguel <74885373+San-Miguel@users.noreply.github.com>
2020-12-04 23:28:22 -0600

    Add files via upload

---
+++ database/seeders/LibroSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; 
+
+class LibroSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('libro')->insert([
+          'nombre'=> 'Estructura de Datos',
+          'autor' => 'Miguel Rodriguez',
+        ]);
+
+        DB::table('libro')->insert([
+          'nombre'=> 'Programacion Orientada a Objetos',
+          'autor' => 'Miguel Rodriguez',
+      ]);
+
+    }
+}
